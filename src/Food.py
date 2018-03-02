78a8c6aace48c44c64d10e3ae65a08286a983c41
Andrey Karyamin <andrey.karyamin@gmail.com>
2018-03-02 10:41:53 +0300

    clean

--- src/Food.py
+++
@@ -1,6 +0,0 @@
-
-class Food:
-    def __init__(self, name, storage_life):
-        self._name = name
-        self._storage_life = storage_life
-        pass
--- src/ProductCatalog.py
+++
@@ -1,7 +0,0 @@
-
-class ProductCatalog:
-    def __init__(self):
-        pass
-
-    def get_price_by_product(self, product_name):
-        pass
